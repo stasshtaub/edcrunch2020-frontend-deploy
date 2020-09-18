6eca0e4783ce919396093c2f2dd7529adde04422
Стас Штауб <shtaubstas@gmail.com>
2020-09-18 14:31:44 +0300

    fix/ Правка слайдера логотипов

--- js/lazyimage.js
+++ js/lazyimage.js
@@ -1,1 +1,41 @@
-if("loading"in HTMLImageElement.prototype){document.querySelectorAll('img[loading="lazy"]').forEach(e=>{e.src=e.dataset.src})}else{const e=document.createElement("script");e.src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js",document.body.appendChild(e)}
+if ("loading" in HTMLImageElement.prototype) {
+	const images = document.querySelectorAll('img[loading="lazy"]');
+	images.forEach((img) => {
+		img.src = img.dataset.src;
+	});
+} else {
+	// Dynamically import the LazySizes library
+	const script = document.createElement("script");
+	script.src =
+		"https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js";
+	document.body.appendChild(script);
+}
+
+{
+	const config = {
+		rootMargin: "50px 0px 50px 0px",
+		threshold: 0,
+	};
+
+	let observer = new IntersectionObserver((entries, self) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				loadImages(entry.target);
+				self.unobserve(entry.target);
+			}
+		});
+	}, config);
+
+	const customLazyElements = document.querySelectorAll(".custom-lazy-load");
+
+	customLazyElements.forEach((el) => {
+		observer.observe(el);
+	});
+
+	const loadImages = (el) => {
+		const images = el.querySelectorAll("img");
+		images.forEach((img) => {
+			img.src = img.dataset.src;
+		});
+	};
+}
