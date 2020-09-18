695c003ed462f77e72900361a075f083fe82557c
Стас Штауб <shtaubstas@gmail.com>
2020-09-18 14:21:27 +0300

    init

---
+++ js/lazyimage.js
@@ -0,0 +1,1 @@
+if("loading"in HTMLImageElement.prototype){document.querySelectorAll('img[loading="lazy"]').forEach(e=>{e.src=e.dataset.src})}else{const e=document.createElement("script");e.src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js",document.body.appendChild(e)}
