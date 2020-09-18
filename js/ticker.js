3a8b0463afb7e0fcd5855808ea22f5809af59857
Стас Штауб <shtaubstas@gmail.com>
2020-09-18 17:45:50 +0300

    js

--- js/ticker.js
+++ js/ticker.js
@@ -1,7 +1,11 @@
 const tickers = document.querySelectorAll('.test-ticker');
+console.log(tickers);
+
 
 tickers.forEach(ticker => {
     let tickerInline = ticker.querySelector('.ticker__inline');
+    console.log(tickerInline);
+    
     let cloneInline = tickerInline.cloneNode(true);
     ticker.append(cloneInline)
 });
