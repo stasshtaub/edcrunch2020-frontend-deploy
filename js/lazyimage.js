9f0eac2319780a8b0438abae3d1fb034e81417cd
Стас Штауб <shtaubstas@gmail.com>
2020-09-29 11:24:55 +0300

    Updates

--- js/lazyimage.js
+++ js/lazyimage.js
@@ -1,1 +1,12 @@
-if("loading"in HTMLImageElement.prototype)document.querySelectorAll('img[loading="lazy"]').forEach(e=>{e.src=e.dataset.src});else{const e=document.createElement("script");e.src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js",document.body.appendChild(e)}
+if ('loading' in HTMLImageElement.prototype) {
+	const images = document.querySelectorAll('img[loading="lazy"]:not(.safari-ignore)');
+	images.forEach((img) => {
+		img.src = img.dataset.src;
+	});
+} else {
+	// Dynamically import the LazySizes library
+	const script = document.createElement('script');
+	script.src =
+		'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
+	document.body.appendChild(script);
+}
